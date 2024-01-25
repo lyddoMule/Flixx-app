6d83d6ab02007be396edacf682cdfdad71cc2473
lidiya mulugeta <lidiyamulugeta94@gmail.com>
2024-01-26 01:25:03 +0300

    movie details feature added

--- js/script.js
+++ js/script.js
@@ -37,11 +37,68 @@ async function displayPopularMovies(){
             </div>`;
 
             grid.appendChild(div)
-            // document.div.appendChild(div);
-            // console.log(div);
-        //   grid.append(div)
+
   })
 
+}
+
+async function displayMovieDetails(){
+    const movieID= window.location.search.split('=')[1 ];
+    const movie= await fetchAPIData(`movie/${movieID}`)
+    // const data= await results.json
+    // console.log(results);
+
+const div= document.createElement('div')
+    div.classList.add('movie-details')
+
+    div.innerHTML=`
+        <div class="details-top">
+            <div>
+
+         ${movie.poster_path?  
+             ` <img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+            />`:`<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+            />`}
+            </div>
+            <div>
+            <h2>${movie.title}</h2>
+            <p>
+                <i class="fas fa-star text-primary"></i>
+                ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+                ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+                ${movie.genres.map((genre)=>
+                    `<li>${genre.name}</li>`).join(' ')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+            </div>
+        </div>
+        <div class="details-bottom">
+            <h2>Movie Info</h2>
+            <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumbers(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumbers(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes </li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+            </ul>
+            <h4>Production Companies</h4>
+            <div class="list-group">
+                    ${movie.production_companies.map((company)=>`${company.name}, `).join(' ')}
+            </div>
+       </div> `
+   
+        document.querySelector('#movie-details').appendChild(div)
 }
 
 async function displayPopularTVShows(){
@@ -77,60 +134,7 @@ async function displayPopularTVShows(){
    })
 }
 
-async function displayMovieDetails(id){
-    const {results}=  await fetchAPIData('movie/popular')
-    const div= document.createElement('div')
-        div.classList.add('movie-details')
 
-    results.forEach((movie)=>{
-        div.innerHTML=`
-        <div class="details-top">
-            <div>
-
-        ${movie.poster_path?  
-             ` <img
-                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                class="card-img-top"
-                alt="${movie.title}"
-            />`:`<img
-            src="../images/no-image.jpg"
-            class="card-img-top"
-            alt="Movie Title"
-            />`}
-            </div>
-            <div>
-            <h2>${movie.title}</h2>
-            <p>
-                <i class="fas fa-star text-primary"></i>
-                8 / 10
-            </p>
-            <p class="text-muted">Release Date: ${movie.release_date}</p>
-            <p>
-                ${movie.overview}
-            </p>
-            <h5>Genres</h5>
-            <ul class="list-group">
-                <li>Genre 1</li>
-                <li>Genre 2</li>
-                <li>Genre 3</li>
-            </ul>
-            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
-            </div>
-        </div>
-        <div class="details-bottom">
-            <h2>Movie Info</h2>
-            <ul>
-            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
-            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
-            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
-            <li><span class="text-secondary">Status:</span> Released</li>
-            </ul>
-            <h4>Production Companies</h4>
-            <div class="list-group">Company 1, Company 2, Company 3</div>
-       </div> `
-    })
-        document.querySelector('#movie-details').appendChild(div)
-}
 
 // Fetch data from tmdb
 async function fetchAPIData(endpoint) {
@@ -146,7 +150,9 @@ async function fetchAPIData(endpoint) {
     return data;
 
 }
-
+function addCommasToNumbers(number) {
+   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
 function showSpinner() {
     document.querySelector('.spinner').classList.add('show')
 }
